a1e28b8650db56c09b22e5b25133fbed7b47a7b3
ahrumnoh <81746529+ahrumnoh@users.noreply.github.com>
2021-07-10 04:55:27 +1000

    change

--- assets/js/forcast.js
+++ assets/js/forcast.js
@@ -14,7 +14,34 @@ const API_Key = "4f2668c377a67ee3025ef5adbb087cea";
 button.addEventListener('click', (event) => {
     event.preventDefault();
 
- 
+    //get input value
+    const cityInput = inputTxt.value;
+    //check console.log
+    //console.log(cityInput);
+
+
+    //Now Fetch Through Get API
+
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+cityInput+'&units=metric&appid=4f2668c377a67ee3025ef5adbb087cea') 
+     
+    
+    
+      .then(res => res.json())
+      .then(data => {
+          console.log(data);
+
+    //when fill input field then clear input field
+            inputTxt.value = " ";
+
+        //Now Show All Date value
+     
+
+
+            showData.innerHTML = `
+
+            
+                 
+                                
 
      });
 });
