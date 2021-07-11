d0a61d66af7a800738e7cf2fde87b753bd1a99a6
ahrumnoh <81746529+ahrumnoh@users.noreply.github.com>
2021-07-11 19:31:47 +1000

    organized

--- assets/js/forcast.js
+++ assets/js/forcast.js
@@ -63,9 +63,17 @@ document.getElementById('currentTime').innerHTML = d.toLocaleString();
 
 //weatherforecast for 5days, Sydney
 
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);
-  js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs)
-  ;}}(document,'script','weatherwidget-io-js');
+!function(d,s,id)
+
+{var js,fjs=d.getElementsByTagName(s)[0];
+
+
+  if(!d.getElementById(id)){js=d.createElement(s);
+
+  js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+  fjs.parentNode.insertBefore(js,fjs);}}
+  
+  (document,'script','weatherwidget-io-js');
 
 
 
