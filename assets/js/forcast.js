d1239758c2b056b65fb64a4e41fe4be1f7fd50d5
ahrum noh <sydneyroseenglish@gmail.com>
2021-07-18 21:55:16 +1000

    Readme file images
    added

--- assets/js/forcast.js
+++ assets/js/forcast.js
@@ -143,21 +143,3 @@ document.getElementById('currentTime').innerHTML = d.toLocaleString();
 
 
 
-//weatherforecast for 5 days in Sydney
-
-!function(d,s,id)
-
-{var js,fjs=d.getElementsByTagName(s)[0];
-
-
-  if(!d.getElementById(id)){js=d.createElement(s);
-
-  js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
-  fjs.parentNode.insertBefore(js,fjs);}}
-  
-  (document,'script','weatherwidget-io-js');
-
-
-
-
-
